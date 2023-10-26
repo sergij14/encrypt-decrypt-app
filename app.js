d8406066654140d41d870541dec41d9e28fabfb1
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-10-26 20:07:45 +0400

    implemented transform stream/piped to next streams

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('log');
+const { Transform } = require("node:stream");
+const fs = require("fs/promises");
+
+class Encrypt extends Transform {
+  _transform(chunk, encoding, cb) {
+    console.log(chunk.toString("utf-8"));
+    this.push(chunk);
+  }
+}
+
+(async () => {
+  const readFileHanlde = await fs.open("read.txt", "r");
+  const writeFileHanlde = await fs.open("write.txt", "w");
+
+  const readStream = readFileHanlde.createReadStream();
+  const writeStream = writeFileHanlde.createWriteStream();
+
+  const encrypt = new Encrypt();
+
+  readStream.pipe(encrypt).pipe(writeStream);
+})();

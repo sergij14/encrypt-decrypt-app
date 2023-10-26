8c80f079c90224c2973e455bfe44627dac058200
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-10-26 20:53:03 +0400

    decrypting the data by modifing buffer

--- app.js
+++ app.js
@@ -12,14 +12,38 @@ class Encrypt extends Transform {
   }
 }
 
+class Decrypt extends Transform {
+    _transform(chunk, encoding, cb) {
+      for (let i = 0; i < chunk.length; i++) {
+        if (chunk[i] !== 255) {
+          chunk[i] = chunk[i] - 1;
+        }
+      }
+      this.push(chunk);
+    }
+  }
+
+// (async () => {
+//   const readFileHanlde = await fs.open("read.txt", "r");
+//   const writeFileHanlde = await fs.open("write.txt", "w");
+
+//   const readStream = readFileHanlde.createReadStream();
+//   const writeStream = writeFileHanlde.createWriteStream();
+
+//   const encrypt = new Encrypt();
+
+//   readStream.pipe(encrypt).pipe(writeStream);
+// })();
+
 (async () => {
-  const readFileHanlde = await fs.open("read.txt", "r");
-  const writeFileHanlde = await fs.open("write.txt", "w");
-
-  const readStream = readFileHanlde.createReadStream();
-  const writeStream = writeFileHanlde.createWriteStream();
-
-  const encrypt = new Encrypt();
-
-  readStream.pipe(encrypt).pipe(writeStream);
-})();
+    const readFileHanlde = await fs.open("write.txt", "r");
+    const writeFileHanlde = await fs.open("decrypted.txt", "w");
+  
+    const readStream = readFileHanlde.createReadStream();
+    const writeStream = writeFileHanlde.createWriteStream();
+  
+    const decrypt = new Decrypt();
+  
+    readStream.pipe(decrypt).pipe(writeStream);
+  })();
+  
